4f76075704ee031b0b56219e85a4e456a59dbbed
axxxkh <alxxxkh@gmail.com>
2022-07-29 20:03:36 +0300

    getting harder

--- cloud-gateway/src/main/java/com/gateway/configuration/AdminAuthenticationFilter.java
+++ cloud-gateway/src/main/java/com/gateway/configuration/AdminAuthenticationFilter.java
@@ -22,6 +22,11 @@ import java.util.List;
 //@NoArgsConstructor
 //@Data
 public class AdminAuthenticationFilter implements GatewayFilter {
+    private String role;
+
+    public AdminAuthenticationFilter authenticationFilter(String role){
+        return new AdminAuthenticationFilter(role);
+    }
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
